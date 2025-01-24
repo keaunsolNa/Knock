3a68560773a4f1446cabb17b5303a2c330e99e6d
keaunsol Na <knsol1992@naver.com>
2025-01-25 01:07:22 +0900

    유저 인덱스 수정

--- src/main/java/org/zerock/knock/dto/document/user/SSO_USER_INDEX.java
+++ src/main/java/org/zerock/knock/dto/document/user/SSO_USER_INDEX.java
@@ -2,12 +2,18 @@ package org.zerock.knock.dto.document.user;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CurrentTimestamp;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.zerock.knock.dto.Enum.AlarmTiming;
+import org.zerock.knock.dto.Enum.CategoryLevelOne;
 import org.zerock.knock.dto.Enum.Role;
 import org.zerock.knock.dto.Enum.SocialLoginType;
+
+import java.sql.Timestamp;
+import java.util.Date;
 
 @Getter
 @Setter
@@ -43,6 +49,17 @@ public class SSO_USER_INDEX {
     @Column(nullable = false)
     private Role role;
 
+    @Enumerated(EnumType.STRING)
+    private CategoryLevelOne favoriteLevelOne;
+
+    @Enumerated(EnumType.STRING)
+    private AlarmTiming[] alarmTimings;
+
+
+    @CurrentTimestamp
+    @Column(nullable = false)
+    private Date lastLoginTime;
+
     @Builder
     public SSO_USER_INDEX(String id, String name, String email, String nickName, String picture, SocialLoginType loginType, Role role) {
         this.id = id;
@@ -52,12 +69,16 @@ public class SSO_USER_INDEX {
         this.picture = picture;
         this.loginType = loginType;
         this.role = role;
+        this.favoriteLevelOne = CategoryLevelOne.MOVIE;
+        this.alarmTimings = new AlarmTiming[] { AlarmTiming.ZERO_HOUR, AlarmTiming.ZERO_HOUR, AlarmTiming.ZERO_HOUR, AlarmTiming.ZERO_HOUR };
+        this.lastLoginTime = new Timestamp(System.currentTimeMillis());
     }
 
     public SSO_USER_INDEX update(String name, String email, String picture) {
         this.name = name;
         this.email = email;
         this.picture = picture;
+        this.lastLoginTime = new Timestamp(System.currentTimeMillis());
 
         return this;
     }
