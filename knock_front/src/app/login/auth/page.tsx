1b654925d9e83fda871624fc11d094dd7fc3edf9
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2025-02-11 01:23:10 +0900

    JWT 토큰 쿠키 저장 테스트 중/redux 추가

--- knock_front/src/app/login/auth/page.tsx
+++ knock_front/src/app/login/auth/page.tsx
@@ -1,9 +1,43 @@
 'use client';
 
+import { useEffect } from 'react';
 import styles from './page.module.scss';
 import { BeatLoader } from 'react-spinners';
 
+/**
+ * 
+ * 백-프론트 통신과정
+
+(/login)
+프론트 로그인 요청 ㅡ> 백 로그인   확인 ㅡ> JWT토큰 생성 / Refresh 토큰 HttpOnly 쿠키로 설정  ㅡ> 프론트 콜백페이지(/login/auth)
+
+(/login/auth)
+프론트 콜백페이지 ㅡ> Access 토큰 요청. fetch 보낼 때 Refresh 토큰 자동전송 ㅡ>  백에서 Access 토큰 body에 담아 response ㅡ> 프론트에서 Redux로 Access Token 저장  ㅡ>  프론트 메인 페이지
+이후 모든 데이터 fetch 시 Access token 담아서 요청
+ */
+
+const getAccessToken = async () => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BACKEND_URL}/auth/getAccessToken`,
+    {
+      method: 'POST',
+      credentials: 'include',
+    }
+  );
+  if (!response.ok) {
+    return <div>통신오류</div>;
+  }
+
+  const data = await response.json();
+
+  console.log(data);
+};
+
 export default function Page() {
+  useEffect(() => {
+    getAccessToken();
+  }, []);
+
   return (
     <div className={styles.container}>
       <BeatLoader size={30} color={'#f45f41'} />
