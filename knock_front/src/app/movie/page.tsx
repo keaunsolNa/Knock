f33e0124f542da4b69c6744b7b08833ff8edc13a
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2025-02-22 03:36:37 +0900

    movie 카테고리 페이지 리팩토링 및 구독 기능 추가

---
+++ knock_front/src/app/movie/page.tsx
@@ -0,0 +1,23 @@
+import { IMovie } from '@/types';
+import SearchBar from '@/components/searchbar/SearchBar';
+import { notFound } from 'next/navigation';
+import ContentList from '@/components/ContentList';
+
+export default async function Page() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BACKEND_URL}/api/movie`
+  );
+
+  if (!response.ok) {
+    notFound();
+  }
+
+  const allMovies: IMovie[] = await response.json();
+
+  return (
+    <>
+      <SearchBar searchTitle="" searchCategory="" />
+      <ContentList itemList={allMovies} category="movie" />
+    </>
+  );
+}
