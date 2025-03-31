9e99d7f96852f073ab23850d99db2b0b027f5741
Yejin Won <78012809+wonyj0228@users.noreply.github.com>
2025-03-31 23:00:57 +0900

    ios 안전영역 배경색상 적용 테스트

--- knock_front/src/app/layout.tsx
+++ knock_front/src/app/layout.tsx
@@ -11,6 +11,9 @@ export const metadata: Metadata = {
   description: '영화, 공연예술을 사랑하는 당신을 위한 티켓팅 헬퍼',
   manifest: '/manifest.json',
   icons: [{ rel: 'icon', url: '/icons/icon512_maskable.png', sizes: '512x512' }],
+  appleWebApp: {
+    statusBarStyle: 'black-translucent',
+  },
 };
 
 export const viewport: Viewport = {
